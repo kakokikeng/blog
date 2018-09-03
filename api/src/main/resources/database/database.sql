f566f292d9a34b118f03e675d5c02b7bf1c56df2
yikang <yikang@nibirutech.com>
2018-09-03 15:28:33 +0800

    database creation

---
+++ api/src/main/resources/database/database.sql
@@ -0,0 +1,93 @@
+create schema blog;
+use blog;
+
+create table blog
+(
+  id            int auto_increment
+  comment '博客id'
+    primary key,
+  create_time   timestamp       not null
+  comment '博客发表时间',
+  read_count    int default '0' not null
+  comment '阅读量',
+  user_id       varchar(32)     not null
+  comment '博主的用户id',
+  title         varchar(20)     not null
+  comment '博客标题',
+  content       text            not null
+  comment '博客正文',
+  like_count    int default '0' not null
+  comment '点赞数量',
+  comment_count int default '0' not null
+  comment '评论数目',
+  constraint create_time_key
+  unique (create_time),
+  constraint user_id_create_time_key
+  unique (user_id, create_time),
+  constraint blog_owner_fk
+  foreign key (user_id) references user (id)
+)
+  comment '博客相关信息';
+
+create table comment
+(
+  id               int auto_increment
+    primary key,
+  user_id          varchar(32)                         not null
+  comment '发表评论用户id',
+  attached_user_id varchar(32)                         null
+  comment '被评论用户id',
+  create_time      timestamp default CURRENT_TIMESTAMP not null
+  comment '评论时间',
+  blog_id          int                                 not null
+  comment '关联博客id
+',
+  content          text                                not null
+  comment '评论内容',
+  constraint comment_user_id_fk
+  foreign key (user_id) references user (id),
+  constraint comment_blog_id_fk
+  foreign key (blog_id) references blog (id)
+);
+
+create index comment_blog_id_fk
+  on comment (blog_id);
+
+create index comment_user_id_fk
+  on comment (user_id);
+
+create table follower
+(
+  id          int auto_increment
+    primary key,
+  user_id     varchar(32) not null
+  comment '用户id
+',
+  follower_id varchar(32) not null
+  comment '关注他的人的id'
+)
+  comment '用户关注者';
+
+create table user
+(
+  id          varchar(32)     not null
+  comment '自增id'
+    primary key,
+  user_name   varchar(10)     not null
+  comment '用户名',
+  email       varchar(20)     not null
+  comment '关联邮箱',
+  fans        int default '0' not null
+  comment '粉丝数量',
+  blogs       int default '0' not null
+  comment '博客数量',
+  create_time timestamp       not null
+  comment '用户创建时间
+',
+  constraint email_user_name_key
+  unique (email, user_name),
+  constraint user_email_uindex
+  unique (email)
+)
+  comment '存储博客用户相关信息';
+
